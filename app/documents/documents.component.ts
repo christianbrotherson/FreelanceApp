830be4374ecdfad25ab689fcf9fe01903773cbcd
Christian Brotherson <christian.brotherson@gmail.com>
2017-04-17 16:24:24 -0600

    Created mock data for the application

--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Document } from './document';
 
 
 @Component({
@@ -7,5 +8,27 @@ import { Component } from '@angular/core';
   templateUrl: 'documents.component.html'
 })
 export class DocumentsComponent {
-
+  documents: Document[] = [
+    {
+      title: "My First Doc",
+      description: "asdfasdf",
+      file_url: 'http://google.com',
+      updated_at: '04/17/17',
+      image_url: 'http://google.com'
+    },
+    {
+      title: "My Second Doc",
+      description: "asdfasdf",
+      file_url: 'http://google.com',
+      updated_at: '04/17/17',
+      image_url: 'http://google.com'
+    },
+    {
+      title: "My Last Doc",
+      description: "asdfasdf",
+      file_url: 'http://google.com',
+      updated_at: '04/17/17',
+      image_url: 'http://google.com'
+    }
+  ]
 }
